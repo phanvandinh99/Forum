d8c576e0bea8e03d1beb0c821481e1347019535b
phanvandinh99 <vonhanh271@gail.com>
2022-02-09 22:42:29 +0700

    Create project

---
+++ Forum/Forum/Areas/BanQuanTri/BanQuanTriAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Forum.Areas.BanQuanTri
+{
+    public class BanQuanTriAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "BanQuanTri";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "BanQuanTri_default",
+                "BanQuanTri/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Forum/Forum/Areas/ThanhVien/ThanhVienAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Forum.Areas.ThanhVien
+{
+    public class ThanhVienAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ThanhVien";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ThanhVien_default",
+                "ThanhVien/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
